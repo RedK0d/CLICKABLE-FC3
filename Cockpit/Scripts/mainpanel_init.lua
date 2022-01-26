729ed893efda29c45f87a55afc71a9b3627238ca
GuillaumeRoyant <94196692+RedK0d@users.noreply.github.com>
2022-01-26 22:47:56 +0100

    Edit Mainpanel
    Mainpanel now only calls the edm file if the plane is correct.

--- Cockpit/Scripts/mainpanel_init.lua
+++ Cockpit/Scripts/mainpanel_init.lua
@@ -1,4 +1,11 @@
-shape_name		   = "SU-27-CLICKABLE"
+
+local  aircraft = get_aircraft_type()
+
+if aircraft=="Su-27"or aircraft=="J-11A" then
+    shape_name		   = "SU-27-CLICKABLE"
+else
+    shape_name		   = ""
+end
 
 
 
